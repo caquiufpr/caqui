1ad45704b75f800fe3b41735219ef3f5d9e44467
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2019-11-21 22:20:00 -0300

    Initial alpha release

--- ru/card.js
+++ ru/card.js
@@ -20,24 +20,88 @@ function generate() {
   var textToWrite = document.getElementById("textarea").value;
   var date = getTheDate();
   var picture = getThePicture();
+  var finaltext = getTheText();
+
   // Draw on Canvas
   var c = document.getElementById("canvas");
   var ctx = c.getContext("2d");
 
   ctx.beginPath();
   ctx.rect(0,0,1080,1920);
-  ctx.fillStyle = "gray";
+  ctx.fillStyle = "white";
   ctx.fill();
-
-  ctx.font = "30px Arial";
-  ctx.fillStyle = "black";
-  ctx.textAlign = "center";
-  ctx.fillText("Hello World", canvas.width/2, canvas.height/2);
 
   ctx.drawImage(picture, 0, 0);
 
+  // date
+  ctx.font = "bold 125px Arial";
+  ctx.fillStyle = "#ff9000";
+  ctx.textAlign = "right";
+  ctx.fillText(date, 978, 445);
+
+  ctx.font = "42px Arial";
+  ctx.fillStyle = "#212121";
+  ctx.textAlign = "left";
+
+  var breakfast = finaltext[0].split("\n");
+  for (var i = 0; i < breakfast.length; i++) {
+    wrapText(ctx, breakfast[i], 110, 650+i*50, 900, 45);
+  }
+
+  var lunch = finaltext[1].split("\n");
+  for (var i = 0; i < lunch.length; i++) {
+    wrapText(ctx, lunch[i], 110, 925+i*50, 900, 45);
+  }
+
+  var dinner = finaltext[2].split("\n");
+  for (var i = 0; i < dinner.length; i++) {
+    wrapText(ctx, dinner[i], 110, 1380+i*50, 900, 45);
+  }
+
   alert("Imagem gerada com sucesso!");
 };
+
+// Code copied on https://codepen.io/peterhry/pen/AGIEa
+
+function wrapText (context, text, x, y, maxWidth, lineHeight) {
+
+    var words = text.split(' '),
+        line = '',
+        i,
+        test,
+        metrics;
+
+    for (i = 0; i < words.length; i++) {
+        test = words[i];
+        metrics = context.measureText(test);
+        while (metrics.width > maxWidth) {
+            // Determine how much of the word will fit
+            test = test.substring(0, test.length - 1);
+            metrics = context.measureText(test);
+        }
+        if (words[i] != test) {
+            words.splice(i + 1, 0,  words[i].substr(test.length))
+            words[i] = test;
+        }
+
+        test = line + words[i] + ' ';
+        metrics = context.measureText(test);
+
+        if (metrics.width > maxWidth && i > 0) {
+            context.fillText(line, x, y);
+            line = words[i] + ' ';
+            y = y + lineHeight;
+            lineCount++;
+        }
+        else {
+            line = test;
+        }
+    }
+
+    context.fillText(line, x, y);
+}
+
+//////////////////////////////////////////////////
 
 var b1 = 0;
 var b2 = 0;
@@ -104,10 +168,36 @@ var content = null;
 function getThePicture() {
   if (b1 == 0) {
     alert("Nenhuma imagem foi selecionada.")
-    stopFUNCTIONHUAAAAAA();
+    stopFUNCTIONHUAAAAAA(huehue);
   }
 
   return document.getElementById("image");
+}
+
+function getTheText() {
+  var text = document.getElementById("textarea").value;
+  text = text.split('\n');
+  var markBreaks = [];
+
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] == "CAF\u00C9 DA MANH\u00C3") {
+      text.splice(i,1);
+    } else if (text[i] == "ALMO\u00C7O") {
+      text.splice(i,1);
+    } else if (text[i] == "JANTAR") {
+      text.splice(i,1);
+    }
+  }
+
+  text = text.join("\n");
+
+  for (var i = 0; i < 5; i++) {
+    text = text.replace("\n\n","<~hue~>")
+  }
+
+  text = text.split("<~hue~>")
+
+  return text;
 }
 
 function download() {
