5646e32042b09f6d58de4c0846b09a1f0f6ea898
Vicente Kalinoski Parmigiani <vicenteparmigiani@gmail.com>
2019-11-23 15:26:53 -0300

    Fix the mess I've made

--- ru/card.js
+++ ru/card.js
@@ -187,9 +187,6 @@ try {
 }
 
 function download() {
-
-  generate();
-
   var download = document.getElementById("download");
   var image = document.getElementById("canvas").toDataURL("image/png")
               .replace("image/png", "image/octet-stream");
