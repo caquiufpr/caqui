85bcc4ca53a3338ed5a0045412676cacc511a242
Vicente Parmigiani <vicenteparmigiani@gmail.com>
2020-04-17 12:26:32 -0300

    Header added

---
+++ bin/script.js
@@ -0,0 +1,32 @@
+var menuOpen = false;
+function openMenu() {
+  const menu = document.getElementById("menu");
+  const menuHolder = document.getElementById("menuHolder");
+  const sandwich = document.getElementById("sandwich");
+
+  if (menuOpen == false) {
+    menu.className = "show";
+    menuHolder.className = "shadow"
+    window.onclick = function() {
+      if (event.target != menu && event.target != sandwich) {
+        menu.className = "";
+        menuHolder.className = ""
+        menuOpen = false;
+      }
+    }
+    menuOpen = true;
+  } else {
+    menu.className = "";
+    menuHolder.className = ""
+    menuOpen = false;
+  }
+}
+
+function headerShadow() {
+	if (document.body.scrollTop == 0 || document.documentElement.scrollTop == 0){
+		document.getElementById("header").className = "headerNoShadow";
+	}
+  if (document.body.scrollTop != 0 || document.documentElement.scrollTop != 0) {
+		document.getElementById("header").className = "";
+	}
+}
